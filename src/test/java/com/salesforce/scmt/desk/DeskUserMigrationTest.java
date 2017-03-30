07893c1e6ad7b340570b879617595c666b9429e6
Thomas Stachl <thomas@stachl.me>
2017-03-30 15:56:40 -0700

    test case for `skipObject`

---
+++ src/test/java/com/salesforce/scmt/desk/DeskUserMigrationTest.java
@@ -0,0 +1,64 @@
+package com.salesforce.scmt.desk;
+
+import com.desk.java.apiclient.model.User;
+import com.salesforce.scmt.service.DeskService;
+import com.salesforce.scmt.utils.DeskUtil;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * Copyright (c) 2017, Salesforce.com, Inc.
+ * All rights reserved.
+ * <p>
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * <p>
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * <p>
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * <p>
+ * Neither the name of Salesforce.com nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+public class DeskUserMigrationTest<D extends Serializable> {
+
+    private DeskUserMigration<com.desk.java.apiclient.model.User> userMigration;
+
+    @Before
+    public void setup() throws Exception {
+        Map<String, String> config = new HashMap<>();
+        this.userMigration = new DeskUserMigration<>(null, config);
+    }
+
+    @Test
+    public void testSkipObject() throws Exception {
+        User skip = new User();
+        skip.setDeleted(true);
+        assertEquals(true, userMigration.skipObject(skip));
+        User migrate = new User();
+        migrate.setDeleted(false);
+        assertEquals(false, userMigration.skipObject(migrate));
+    }
+
+}
