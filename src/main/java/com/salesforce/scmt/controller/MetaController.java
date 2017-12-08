99fae7cbe1d046b8186c06f0f57b4ae9f7d14965
Eric Dahl <eric@skeyelab.com>
2017-12-08 15:37:33 -0600

    Adding MetaData Endpoint

--- src/main/java/com/salesforce/scmt/controller/MetaController.java
+++ src/main/java/com/salesforce/scmt/controller/MetaController.java
@@ -5,28 +5,19 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-
 package com.salesforce.scmt.controller;
 
 import static spark.Spark.post;
 
-//import com.salesforce.scmt.service.DeskService;
+import com.salesforce.scmt.service.SalesforceService;
 import com.salesforce.scmt.utils.JsonTransformer;
 
-public class MetaController
-{
-    private static final String PREFIX_URI = "/meta";
+public class MetaController {
+  private static final String PREFIX_URI = "/meta";
 
-    public MetaController()
-    {
-        post(PREFIX_URI + "/remotesite", (req, res) -> { return SalesforceService.createRemoteSite(req, res); }, new JsonTransformer() );
-        
-    //     post(PREFIX_URI + "/AuthenticateTokens", (req, res) -> { return DeskService.authenticateTokens(req, res); }, new JsonTransformer());
-    //     post(PREFIX_URI + "/MigrateMetadata", (req, res) -> { return DeskService.migrateMetadata(req, res); }, new JsonTransformer());
-    //     post(PREFIX_URI + "/MigrateData", (req, res) -> { return DeskService.migrateData(req, res); }, new JsonTransformer());
-    //     post(PREFIX_URI + "/MigrateAttachments", (req, res) -> { return DeskService.migrateAttachments(req, res); }, new JsonTransformer());
-
-    //     post(PREFIX_URI + "/retrieveMetadata", (req, res) -> { return DeskService.retrieveMetadata(req, res); }, new JsonTransformer());
-    // 
-    }
+  public MetaController() {
+    post(PREFIX_URI + "/remotesite", (req, res) -> {
+      return SalesforceService.createRemoteSite(req, res);
+    }, new JsonTransformer());
+  }
 }
