f620f8febc5dc922b2dd4e0ef486526afedd3977
Eric Dahl <eric@skeyelab.com>
2017-12-11 15:58:26 -0600

    Revert "Adding MetaController and endpoint to Main.java"
    This reverts commit abfe4345183c62970377b368cecb338220e37b56.

--- src/main/java/com/salesforce/scmt/Main.java
+++ src/main/java/com/salesforce/scmt/Main.java
@@ -24,99 +24,101 @@ import static spark.Spark.*;
 
 import java.io.IOException;
 
+
 import static com.salesforce.scmt.utils.JsonUtil.toJson;
-import com.salesforce.scmt.utils.Utils;
 
 import com.salesforce.scmt.model.ErrorResponse;
 import com.salesforce.scmt.utils.Utils;
 
 //import com.heroku.sdk.jdbc.DatabaseUrl;
 
-public final class Main {
-	/**
-	 * Private constructor for program start.
-	 */
-	private Main() {
-	}
+public final class Main
+{
+    /**
+     * Private constructor for program start.
+     */
+    private Main() {}
 
-	public static void main(String[] args) {
-		/**
-		 * By default, Spark runs on port 4567. If you want to set another port use port. This has to be done before
-		 * using routes and filters:
-		 */
-		port(Integer.valueOf(System.getenv("PORT")));
+    public static void main(String[] args)
+    {
+        /**
+         * By default, Spark runs on port 4567. If you want to set another port use port. This has to be done before
+         * using routes and filters:
+         */
+        port(Integer.valueOf(System.getenv("PORT")));
 
-		/**
-		 * You can assign a folder in the classpath serving static files with the staticFileLocation method. Note that
-		 * the public directory name is not included in the URL.
-		 * A file /public/css/style.css is made available as http://{host}:{port}/css/style.css
-		 */
-		staticFileLocation("/public");
+        /**
+         * You can assign a folder in the classpath serving static files with the staticFileLocation method. Note that
+         * the public directory name is not included in the URL.
+         * A file /public/css/style.css is made available as http://{host}:{port}/css/style.css
+         */
+        staticFileLocation("/public");
 
-		// Before-filters are evaluated before each request, and can read the request and read/modify the response.
-		before((request, response) -> {
-			// default the response type to JSON
-			response.type("text/json");
+        // Before-filters are evaluated before each request, and can read the request and read/modify the response.
+        before((request, response) -> {
+            // default the response type to JSON
+            response.type("text/json");
 
-			// set the 'Access-Control-Allow-Origin' for CORS
-			response.header("Access-Control-Allow-Origin", "*");
+            // set the 'Access-Control-Allow-Origin' for CORS
+            response.header("Access-Control-Allow-Origin", "*");
 
-			// set the 'X-XSS-Protection'
-			response.header("X-XSS-Protection", "1");
+            // set the 'X-XSS-Protection'
+            response.header("X-XSS-Protection", "1");
 
-			// force to https
-			System.out.println(request.headers("x-forwarded-proto"));
-			if (!"https".equalsIgnoreCase(request.headers("x-forwarded-proto"))) {
+            // force to https
+            System.out.println(request.headers("x-forwarded-proto"));
+        	if(!"https".equalsIgnoreCase(request.headers("x-forwarded-proto"))){
 
-				// allow local development without ssl
-				if (!"127.0.0.1".equalsIgnoreCase(request.ip())) {
-					throw new UnsupportedOperationException("only http alllowd");
-				}
+        		// allow local development without ssl
+        		if(!"127.0.0.1".equalsIgnoreCase(request.ip()))
+        		{
+        			throw new UnsupportedOperationException("only http alllowd");
+        		}
 
-			}
-		});
+        	}
+        });
 
-		// After-filters are evaluated after each request, and can read the request and read/modify the response
-		after((request, response) -> {
-			// GZIP compress everything
-			response.header("Content-Encoding", "gzip");
-		});
+        // After-filters are evaluated after each request, and can read the request and read/modify the response
+        after((request, response) -> {
+            // GZIP compress everything
+            response.header("Content-Encoding", "gzip");
+        });
 
-		/**
-		 * Register an exception handler when an 'IllegalArgumentException' is thrown.
-		 */
-		exception(IllegalArgumentException.class, (e, req, res) -> {
-			res.status(400);
-			res.body(toJson(new ErrorResponse(e)));
-		});
+        /**
+         * Register an exception handler when an 'IllegalArgumentException' is thrown.
+         */
+        exception(IllegalArgumentException.class, (e, req, res) -> {
+            res.status(400);
+            res.body(toJson(new ErrorResponse(e)));
+            });
 
-		/**
-		 * Register an exception handler when an 'IOException' is thrown.
-		 */
-		exception(IOException.class, (e, req, res) -> {
-			res.status(400);
-			res.body(toJson(new ErrorResponse(e)));
-		});
+        /**
+         * Register an exception handler when an 'IOException' is thrown.
+         */
+        exception(IOException.class, (e, req, res) -> {
+            res.status(400);
+            res.body(toJson(new ErrorResponse(e)));
+            });
+        
+        /**
+         * Register an exception handler when an 'Exception' is thrown.
+         */
+        exception(UnsupportedOperationException.class, (e, req, res) -> {
+            res.status(403);
+            res.body(toJson(new ErrorResponse(e)));
+            });
 
-		/**
-		 * Register an exception handler when an 'Exception' is thrown.
-		 */
-		exception(UnsupportedOperationException.class, (e, req, res) -> {
-			res.status(403);
-			res.body(toJson(new ErrorResponse(e)));
-		});
+        /**
+         * Register an exception handler when an 'Exception' is thrown.
+         */
+        exception(Exception.class, (e, req, res) -> {
+            res.status(400);
+            res.body(toJson(new ErrorResponse(e)));
+            });
 
-		/**
-		 * Register an exception handler when an 'Exception' is thrown.
-		 */
-		exception(Exception.class, (e, req, res) -> {
-			res.status(400);
-			res.body(toJson(new ErrorResponse(e)));
-		});
 
-		// add the desk end-points
-		new com.salesforce.scmt.controller.DeskController();
-		new com.salesforce.scmt.controller.MetaController();
-		new com.salesforce.scmt.controller.KbController();
-	}
+
+        // add the desk end-points
+        new com.salesforce.scmt.controller.DeskController();
+    }
 }
